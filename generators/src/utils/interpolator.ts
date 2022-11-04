2419ab05c0b7f9094262d441eb2c62009cec0393
Mario Arnautou <mario.arnautou@gmail.com>
2022-11-04 09:54:04 +0100

    feat(generators): support nested object JSON-path syntax in interpolations

--- generators/src/utils/interpolator.ts
+++ generators/src/utils/interpolator.ts
@@ -1,8 +1,19 @@
 export const interpolate = (str: string, inputs: Record<string, unknown>): string => {
   let interpolated = str;
-  const matches = interpolated.matchAll(/\${input:([a-zA-Z0-9_-]+)}/g);
+  const matches = interpolated.matchAll(/\${input:([.a-zA-Z0-9_-]+)}/g);
   for (const match of matches) {
-    interpolated = interpolated.replace(match[0], inputs[match[1]].toString());
+    let value: unknown = inputs;
+    const attrs = match[1].split('.');
+    for (const attr of attrs) {
+      if (typeof value !== 'object' || !Object.keys(value).includes(attr)) {
+        throw new Error(`Incorrect JSON path ${match[1]}`);
+      }
+      value = (value as Record<string, unknown>)[attr];
+    }
+    if (typeof value !== 'string') {
+      throw new Error(`Incorrect JSON path ${match[1]}`);
+    }
+    interpolated = interpolated.replace(match[0], value);
   }
   return interpolated;
 };
