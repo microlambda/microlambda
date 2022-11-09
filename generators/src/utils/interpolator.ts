63c7725565011add1bafccf34eafcb5797720a46
Mario Arnautou <mario.arnautou@gmail.com>
2022-11-09 10:32:47 +0100

    fix(generators): do not use NTFS-incompatible chars in interpolation syntax

--- generators/src/utils/interpolator.ts
+++ generators/src/utils/interpolator.ts
@@ -1,19 +1,23 @@
+const isPrimitive = (value: unknown): boolean => {
+  return ['number', 'boolean', 'string'].includes(typeof value);
+}
+
 export const interpolate = (str: string, inputs: Record<string, unknown>): string => {
   let interpolated = str;
-  const matches = interpolated.matchAll(/\${input:([.a-zA-Z0-9_-]+)}/g);
+  const matches = interpolated.matchAll(/\${input\.([.a-zA-Z0-9_-]+)}/g);
   for (const match of matches) {
     let value: unknown = inputs;
     const attrs = match[1].split('.');
     for (const attr of attrs) {
       if (typeof value !== 'object' || !Object.keys(value).includes(attr)) {
-        throw new Error(`Incorrect JSON path ${match[1]}`);
+        throw new Error(`Incorrect JSON path: ${match[1]} does not exist`);
       }
       value = (value as Record<string, unknown>)[attr];
     }
-    if (typeof value !== 'string') {
-      throw new Error(`Incorrect JSON path ${match[1]}`);
+    if (!isPrimitive(value)) {
+      throw new Error(`Incorrect JSON path: ${match[1]} is not a primitive`);
     }
-    interpolated = interpolated.replace(match[0], value);
+    interpolated = interpolated.replace(match[0], value.toString());
   }
   return interpolated;
 };
