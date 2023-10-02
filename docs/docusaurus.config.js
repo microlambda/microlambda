efc7cb906acec9c6dced76513dbf468169abda1e
Mario Arnautou <mario.arnautou@gmail.com>
2023-10-02 17:07:53 +0200

    docs: update footer

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -100,15 +100,7 @@ const config = {
             items: [
               {
                 label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://stackoverflow.com/questions/tagged/microlambda',
               },
             ],
           },
@@ -121,7 +113,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/microlambda/microlambda',
               },
             ],
           },
