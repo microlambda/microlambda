e3e9eb9c0625a05b810023ac5ddadb988802d6b2
Thibault Ruby <thibault.ruby@neoxia.com>
2023-11-08 17:27:01 +0100

    fix: avoid runtime error due to undefined

--- plugin/src/features/transforms/deployment-role.ts
+++ plugin/src/features/transforms/deployment-role.ts
@@ -1,8 +1,14 @@
-import {ServerlessInstance} from "@microlambda/types";
-import {IRootConfig} from "@microlambda/config";
+import { ServerlessInstance } from '@microlambda/types';
+import { IRootConfig } from '@microlambda/config';
 
-export const updateDeploymentRole = (serverless: ServerlessInstance, config?: IRootConfig): void => {
-  if (!serverless.service.provider.iam.deploymentRole && config?.deploymentRole) {
+export const updateDeploymentRole = (
+  serverless: ServerlessInstance,
+  config?: IRootConfig,
+): void => {
+  if (
+    !serverless?.service?.provider?.iam?.deploymentRole &&
+    config?.deploymentRole
+  ) {
     serverless.service.provider.iam.deploymentRole = config.deploymentRole;
   }
-}
+};
